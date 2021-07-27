02c5d740815027b45b552caaa6de405302c963ed
LG <76845820+coder-lg@users.noreply.github.com>
2021-07-27 17:22:25 +0530

    fail, gotta change this

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -5,3 +5,7 @@ import { Footer as _Footer } from '@codedoc/core/components';
 export function Footer(config: CodedocConfig, renderer: any) {
   return <_Footer/>;
 }
+<a href="https://github.com/CONNECT-platform/coding-blog" target="_blank" class={classes.imgLink}>
+      <img src="/img/social/github-light.svg" class="light"/>
+      <img src="/img/social/github-dark.svg" class="dark"/>
+    </a>
