59346d29057111ced93a4e97f44fbd0f0e91ca14
LG <76845820+coder-lg@users.noreply.github.com>
2021-07-27 17:54:07 +0530

    test 1

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -1,11 +1,60 @@
-import { CodedocConfig } from '@codedoc/core';
+import { CodedocConfig, CodedocTheme } from '@codedoc/core';
 import { Footer as _Footer } from '@codedoc/core/components';
+import { themedStyle, ThemedComponentThis } from '@connectv/jss-theme';
 
 
-export function Footer(config: CodedocConfig, renderer: any) {
-  return <_Footer/>;
-}
-<a href="https://github.com/coder-lg" target="_blank" class={classes.imgLink}>
+const style = themedStyle<CodedocTheme>(() => ({
+  imgLink: {
+    position: 'relative',
+    width: 24,
+    height: 24,
+    margin: '0 12px',
+    opacity: .25,
+    transition: 'opacity .1s !important',
+    '&:hover': { opacity: 1, },
+
+    '& img': {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      transition: 'opacity .3s',
+
+      '&.light': {
+        'body.dark &': {
+          opacity: 0,
+        },
+    
+        '@media (prefers-color-scheme: dark)': {
+          'body:not(.dark-mode-animate) &': {
+            opacity: 0
+          }
+        },
+      },
+
+      '&.dark': {
+        opacity: 0,
+        'body.dark &': {
+          opacity: 1,
+        },
+    
+        '@media (prefers-color-scheme: dark)': {
+          'body:not(.dark-mode-animate) &': {
+            opacity: 1
+          }
+        },
+      },
+    }
+  }
+}));
+
+
+export function Footer(this: ThemedComponentThis, config: CodedocConfig, renderer: any) {
+  const classes = this.theme.classes(style);
+  return <_Footer>
+    <a href="https://github.com/CONNECT-platform/coding-blog" target="_blank" class={classes.imgLink}>
       <img src="/src/assets/images/github-light.svg" class="light"/>
       <img src="/src/assets/images/github-dark.svg" class="dark"/>
-    </a>
+  </_Footer>;
+}
