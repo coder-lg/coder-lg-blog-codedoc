c3a9f93bfcc0033d82042f8c116faf0c90a9b3cb
LG <76845820+coder-lg@users.noreply.github.com>
2021-07-30 09:55:18 +0530

    Update footer.tsx

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -1,7 +1,11 @@
 import { CodedocConfig } from '@codedoc/core';
-import { Footer as _Footer } from '@codedoc/core/components';
+import { Footer as _Footer, Icon } from '@codedoc/core/components';
 
 
 export function Footer(config: CodedocConfig, renderer: any) {
-  return <_Footer/>;
+  return <_Footer>
+    <a href="https://github.com/coder-lg" target="_blank">GitHub</a>
+    <hr/>
+    <a href="https://coderlg.coding.blog/_feed.rss" target="_blank"><Icon>rss_feed</Icon>RSS</a>
+  </_Footer>;
 }
