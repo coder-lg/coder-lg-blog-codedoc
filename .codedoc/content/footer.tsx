0e1d049aec99306cea63df7ddc8b7a87007763e0
Hex <balovijayagan@gmail.com>
2021-08-19 16:40:15 +0000

    Initial Commit

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -1,11 +1,62 @@
-import { CodedocConfig } from '@codedoc/core';
-import { Footer as _Footer, Icon } from '@codedoc/core/components';
+import { CodedocConfig, CodedocTheme } from '@codedoc/core';
+import { Footer as _Footer } from '@codedoc/core/components';
+import { themedStyle, ThemedComponentThis } from '@connectv/jss-theme';
 
 
-export function Footer(config: CodedocConfig, renderer: any) {
+const style = themedStyle<CodedocTheme>(() => ({
+  imgLink: {
+    position: 'relative',
+    width: 24,
+    height: 24,
+    margin: '0 12px',
+    opacity: .25,
+    transition: 'opacity .1s !important',
+    '&:hover': { opacity: 1, },
+
+    '& img': {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: 0,
+      left: 0,
+      transition: 'opacity .3s',
+
+      '&.light': {
+        'body.dark &': {
+          opacity: 0,
+        },
+    
+        '@media (prefers-color-scheme: dark)': {
+          'body:not(.dark-mode-animate) &': {
+            opacity: 0
+          }
+        },
+      },
+
+      '&.dark': {
+        opacity: 0,
+        'body.dark &': {
+          opacity: 1,
+        },
+    
+        '@media (prefers-color-scheme: dark)': {
+          'body:not(.dark-mode-animate) &': {
+            opacity: 1
+          }
+        },
+      },
+    }
+  }
+}));
+
+
+export function Footer(this: ThemedComponentThis, config: CodedocConfig, renderer: any) {
+  const classes = this.theme.classes(style);
   return <_Footer>
-    <a href="https://github.com/im-coder-lg" target="_blank">GitHub</a>
-    <hr/>
-    <a href="https://coderlg.coding.blog/_feed.rss" target="_blank"><Icon>rss_feed</Icon>RSS</a>
+    <a href="https://github.com/CONNECT-platform/coding-blog" target="_blank" class={classes.imgLink}>
+      <img src="/img/social/github-light.svg" class="light"/>
+      <img src="/img/social/github-dark.svg" class="dark"/>
+    </a>
+    <a href="https://coderlg.coding.blog/_feed.rss" target="_blank">RSS</a>
   </_Footer>;
 }
