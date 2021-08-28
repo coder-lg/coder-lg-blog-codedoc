1d024e8b0770ea53cb4713a3ef999f11a956e3ef
Hex <balovijayagan@gmail.com>
2021-08-28 03:42:01 +0000

    Add node part to Dockerfile

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -16,7 +16,7 @@ FROM gitpod/workspace-full
 
 # Install custom tools, runtime, etc.
 
-# RUN bash .gitpod/gitpodDockerfile.sh
+RUN bash .gitpod/gitpodDockerfile.sh
 
 # RUN sudo apt-get install curl software-properties-common 
 # RUN curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - 
--- .gitpod/gitpodDockerfile.sh
+++ .gitpod/gitpodDockerfile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-npm i -g @codedoc/cli
-
-codedoc install
-
-codedoc serve
+sudo apt-get -y update
+sudo apt-get -y install curl software-properties-common 
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash - 
+sudo apt-get -y install nodejs 
+node --version
+npm --version
